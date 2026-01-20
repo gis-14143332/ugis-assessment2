934505a4969cabd629d810d049767f21f925cff0
gis-14143332 <1285912756@qq.com>
2026-01-20 04:50:02 +0000

    random point
    drop a random point in study area:
    1.find boundary of this shape
    2.try until point is in this shape
    3.choose random xy in frame
    4.if in study area ,choose it

--- assessment2.py
+++ assessment2.py
@@ -15,12 +15,24 @@ start_time = perf_counter()
 import os
 import geopandas
 import rasterio
+import random
+from shapely.geometry import Point, Polygon, MultiPolygon
+
+#basic rule
+map_standard = "EPSG:3857"
+
 
 #find data
 my_data_folder = os.path.dirname(os.path.abspath(__file__)) #find which file this code in
 def find_my_file(name): 
     return os.path.join(my_data_folder, 'data', 'wr', name) 
 
+#random point in study area
+def make_random_dot(area_shape): 
+    left, bottom, right, top = area_shape.bounds # 找到这个形状的最外圈边界
+    while True: # 不断尝试，直到点掉进形状里
+        p = Point(random.uniform(left, right), random.uniform(bottom, top)) # 在方框里随机选坐标
+        if area_shape.contains(p): return p # 如果点在行政区内，就选定它
 
 
 
