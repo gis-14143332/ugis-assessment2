f7a563230acc5ee3136934afa5a6c6463157e606
gis-14143332 <1285912756@qq.com>
2026-01-20 04:40:51 +0000

    find data

--- assessment2.py
+++ assessment2.py
@@ -1,6 +1,6 @@
 """
 Understanding GIS: Assessment 2
-@author [INSERT STUDENT NUMBER HERE]
+@author [14143332]
 """
 from time import perf_counter
 
@@ -11,6 +11,17 @@ start_time = perf_counter()
 
 
 ''' --- ALL CODE MUST BE INSIDE HERE --- '''
+#import
+import os
+import geopandas
+import rasterio
+
+#find data
+my_data_folder = os.path.dirname(os.path.abspath(__file__)) #find which file this code in
+def find_my_file(name): 
+    return os.path.join(my_data_folder, 'data', 'wr', name) 
+
+
 
 
 # --- NO CODE BELOW HERE ---
