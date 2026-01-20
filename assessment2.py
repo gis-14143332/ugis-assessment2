1ce45746ad42bc07caaf838eb2600ab9ca9406c7
gis-14143332 <1285912756@qq.com>
2026-01-20 04:55:11 +0000

    check population
    1.transfer lon&lat to xy
    2.check number

--- assessment2.py
+++ assessment2.py
@@ -29,12 +29,20 @@ def find_my_file(name):
 
 #random point in study area
 def make_random_dot(area_shape): 
-    left, bottom, right, top = area_shape.bounds # 找到这个形状的最外圈边界
-    while True: # 不断尝试，直到点掉进形状里
-        p = Point(random.uniform(left, right), random.uniform(bottom, top)) # 在方框里随机选坐标
-        if area_shape.contains(p): return p # 如果点在行政区内，就选定它
-
-
+    left, bottom, right, top = area_shape.bounds # find boundary
+    while True: # try until in shape
+        p = Point(random.uniform(left, right), random.uniform(bottom, top)) # random position in square
+        if area_shape.contains(p): return p # if in study area,choose
+        
+#check this point's population data
+def look_at_population(p, raster_file, raster_data): 
+    try:
+        row, col = raster_file.index(p.x, p.y) # transfer longtitude&lattitude to xy
+        # check number
+        if 0 <= row < raster_file.height and 0 <= col < raster_file.width:
+            return max(0, float(raster_data[row, col])) 
+        return 0.0
+    except: return 0.0 #if wrong
 
 # --- NO CODE BELOW HERE ---
 
